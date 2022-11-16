4bf0b590d8b3af6872bdc2bc50972e3bff9c81a1
Hao <hao-le@live.co.uk>
2022-11-16 14:08:18 +0000

    added the repetition statement

--- src/main/java/com/nology/numbergame/NumberGame.java
+++ src/main/java/com/nology/numbergame/NumberGame.java
@@ -1,4 +1,7 @@
 package com.nology.numbergame;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -12,33 +15,52 @@ public class NumberGame {
         int number = 0;
         int random_int = (int) Math.floor(Math.random() * (max - min + 1) + min);
         Scanner myObj = new Scanner(System.in);
+        System.out.println(random_int);
+
+
+        ArrayList<Integer> ans = new ArrayList<>();
+
 
         while (!correct) {
             System.out.println("You are limited to " + tries + " attempts."
                     + " Guess a number between 1 and 100: ");
             tries--;
             number = myObj.nextInt();
-                if (number == random_int) {
-                    System.out.println("correct! you win :)");
-                    correct = true;
-                }  else if (tries == 0) {
-                    System.out.println("game over!");
-                    correct = true;
-                } else if (number <= random_int + 3 && number >= random_int - 3) {
-                    System.out.println("Very very warm");
-                } else if (number <= random_int + 6 && number >= random_int - 6) {
-                    System.out.println("Very warm");
-                } else if (number <= random_int + 10 && number >= random_int - 10) {
-                    System.out.println("Warm");
-                } else if (number <= random_int + 16 && number >= random_int - 16) {
-                    System.out.println("Cold");
-                } else if (number <= random_int + 21 && number >= random_int - 21) {
-                    System.out.println("Very very warm");
-                } else if (number <= random_int + 25 && number >= random_int - 25) {
-                    System.out.println("freezing");
-                } else if (number <= random_int + 30 && number >= random_int - 30) {
-                    System.out.println("Artic");
-                } else System.out.println("No where near");
+            boolean alreadyExist = false;
+            if (!alreadyExist) {
+                for (Integer integer : ans) {
+                    if (integer.equals(number)) {
+                        alreadyExist = true;
+                    }
+                }
+                if (alreadyExist == false) {
+                    ans.add(number);
+                } else {
+                    System.out.println("this number has already been used");
+                    tries++;
+                }
             }
+            if (number == random_int) {
+                System.out.println("correct! you win :)");
+                correct = true;
+            } else if (tries == 0) {
+                System.out.println("game over!");
+                correct = true;
+            } else if (number <= random_int + 3 && number >= random_int - 3) {
+                System.out.println("Very very warm");
+            } else if (number <= random_int + 6 && number >= random_int - 6) {
+                System.out.println("Very warm");
+            } else if (number <= random_int + 10 && number >= random_int - 10) {
+                System.out.println("Warm");
+            } else if (number <= random_int + 16 && number >= random_int - 16) {
+                System.out.println("Cold");
+            } else if (number <= random_int + 21 && number >= random_int - 21) {
+                System.out.println("Very very warm");
+            } else if (number <= random_int + 25 && number >= random_int - 25) {
+                System.out.println("freezing");
+            } else if (number <= random_int + 30 && number >= random_int - 30) {
+                System.out.println("Artic");
+            } else System.out.println("No where near");
         }
     }
+}
